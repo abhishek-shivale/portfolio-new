f20805ee8c939e3a0a0b4f7ed0e70ba324ee7e2e
abhishek-shivale <abhishekshivale45@gmail.com>
2024-12-09 14:54:11 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -277,7 +277,7 @@ const PortfolioWebsite = () => {
 
   return (
     <div className="min-h-screen bg-black text-white">
-      <div className="container mx-auto px-4 py-12 max-w-5xl">
+      <div className=" max-w-5xl">
         {/* Navigation */}
         <div className="flex justify-center mb-12">
           <div className="bg-neutral-800 rounded-full p-2 flex space-x-2 border border-neutral-700">
